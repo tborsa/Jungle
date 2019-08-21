e5b8155172ad8b442fd6962c05caec9f5340640a
kir- <graysonclee@gmail.com>
2019-08-21 21:45:30 +0000

    added category and product count on admin dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
+    @products = Product.all.count
+    @categories = Category.all.count
+    @orders = Order.all.count
   end
 end
